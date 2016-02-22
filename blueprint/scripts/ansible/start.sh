f046f1c1c628f3d2d9abdd5ef7fda6c5e95ffe62
Assi Maimon <maimon33@gmail.com>
2016-02-22 15:18:23 +0200

    Back to static palybook filename

--- blueprint/scripts/ansible/start.sh
+++ blueprint/scripts/ansible/start.sh
@@ -21,12 +21,10 @@ set +e
 
 
 TEMP_DIR=`mktemp -d`
-ctx logger info "temp directory ${TEMP_DIR}"
 ANSIBLE_DIRECTORY=${TEMP_DIR}/$(ctx execution-id)/ansible
-# replace FILENAME with 'PLAYBOOK_FILENAME'
-PLAYBOOK_FILENAME=$(ctx instance properties playbook_file_name)
+PLAYBOOK_FILENAME=main.yaml
 PLAYBOOK_PATH=${ANSIBLE_DIRECTORY}/${PLAYBOOK_FILENAME}
-ctx logger info "playbook name: ${PLAYBOOK_FILENAME}"
+ctx logger info "tmp folder here: ${ANSIBLE_DIRECTORY}"
 
 mkdir -p ${ANSIBLE_DIRECTORY}/roles
 
@@ -34,18 +32,15 @@ mkdir -p ${ANSIBLE_DIRECTORY}/roles
 TEMP_CONF_PATH=$(ctx download-resource-and-render resources/ansible.cfg)
 TEMP_VAR_PATH=$(ctx download-resource-and-render resources/default.yml)
 
-ctx logger info "Downloaded ansible.cfg to ${TEMP_CONF_PATH}"
 CONF_PATH=$ANSIBLE_DIRECTORY/ansible.cfg
 VAR_PATH=$ANSIBLE_DIRECTORY/default.yml
 INVENTORY_PATH=$ANSIBLE_DIRECTORY/inventory
 cp $TEMP_CONF_PATH $CONF_PATH
 cp $TEMP_VAR_PATH $VAR_PATH
-ctx logger info "Copied ${TEMP_CONF_PATH} ${CONF_PATH}"
 export ANSIBLE_CONFIG=${CONF_PATH}
 ctx instance runtime-properties confpath ${CONF_PATH}
 
 # Add the ansible hostname name to the inventory and to etc hosts
-#INVENTORY_FILE=$(ctx download-resource-and-render resources/inventory { "application_host_public_ip": "$application_host_public_ip" })
 INVENTORY_FILE=$(ctx download-resource-and-render resources/inventory)
 cp $INVENTORY_FILE $INVENTORY_PATH
 rpx repl -p $INVENTORY_PATH -r HOST -w $application_host_public_ip
@@ -63,6 +58,5 @@ rpx repl -p $PLAYBOOK_PATH -r butterfly_pid_stdout -w {{butterfly_PID.stdout}}
 rpx repl -p $PLAYBOOK_PATH -r Ansible_host_external_ip -w {{ext_ip.stdout}}
 
 # Run playbook after manipulation
-sleep 1m
 ansible-playbook ${PLAYBOOK_PATH} > ${ANSIBLE_DIRECTORY}/output.log 2>&1
 ctx logger info "Executed ${PLAYBOOK_PATH}"
